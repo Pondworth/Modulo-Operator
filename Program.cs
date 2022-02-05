2f9ac82274c2edd247359e4b53c77d6f9d9e1160
Pondworth <amy@pondworth.de>
2022-02-05 16:59:58 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Modulo Operator (Restwert Operator)
+            int zahl = 6 % 4; //1 Rest:2
+            int zahl2 = 10 % 3; //3 Rest: 1
+
+            Console.WriteLine(zahl);
+            Console.ReadKey();
+        }
+    }
+}
